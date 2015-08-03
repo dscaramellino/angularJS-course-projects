6d301f8a6149ed2abfb6ab3de470f82613a51513
Danielle Scaramellino <dscaramellino@newvisions.org>
2015-08-02 22:57:41 -0400

    Completes ui-theme-picker homework assignment

---
+++ homework/ui-theme-picker/app.js
@@ -0,0 +1,31 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', function() {
+  var ctrl = this;
+
+  ctrl.selectedTheme = "None";
+  ctrl.fileName = undefined;
+  ctrl.selectedFolder = undefined;
+  ctrl.hideFiles = true;
+  ctrl.themesArr = ["None", "Primary", "Info", "Success", "Warning", "Danger"];
+  ctrl.themesObj = {
+    "None": undefined,
+    "Primary": "bg-primary",
+    "Info": "bg-info",
+    "Success": "bg-success",
+    "Warning": "bg-warning",
+    "Danger": "bg-danger"
+  };
+  ctrl.foldersObj = {
+    "Personal": [],
+    "Work": [],
+    "Trips": []
+  };
+  ctrl.addFile = function() {
+    if (ctrl.fileName&&ctrl.selectedFolder) {
+      ctrl.foldersObj[ctrl.selectedFolder].push(ctrl.fileName);
+      ctrl.fileName = undefined;
+      ctrl.selectedFolder = undefined;
+    }
+  }
+});
